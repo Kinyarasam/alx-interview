891cd95443b35bda9f3ce3287a9463cc46d27136
kinyarasam <skinyara.30@gmail.com>
2023-02-26 20:39:30 +0300

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from typing import List
+
+
+def makeChange(coins: List, total: int) -> int:
+    """Returns the fewest number of coins needed to meet the given amount.
+
+    Args:
+        coins (List): List of values of the coins in your possession.
+        total (int): The total amount which you need to make change.
+
+    Returns:
+        (int) - The fewest number of coins needed to meet total
+    """
+    if total < 1:
+        return 0
+
+    # Initialize the dynamic programming table.
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    # Fill the dynamic programming table
+    for i in range(1, total + 1):
+        for j in range(len(coins)):
+            if coins[j] <= i:
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+
+    # return the result
+    return dp[total] if dp[total] != float('inf') else -1
