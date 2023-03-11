295ba0f422a6ebb22ab20a95bcd78af6e2e70ab9
kinyarasam <skinyara.30@gmail.com>
2023-03-11 18:52:45 +0300

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Rotate a 2D matrix"""
-from typing import List
 
 
-def rotate_2d_matrix(matrix: List) -> None:
+def rotate_2d_matrix(matrix):
     """
     Rotates a 2D matrix
 
     Args:
+        matrix (List): the 2D matrix
     """
     # print([list(reversed(col)) for col in zip(*matrix)])
     # return [list(reversed(col)) for col in zip(*matrix)]
